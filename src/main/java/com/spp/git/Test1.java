2badba2587d9341ad7dfc96b3b2eaffbec3bacdc
AssassinShang <shang_pan@126.com>
2019-07-14 20:57:33 +0800

    server encoding
    server encoding

--- src/main/java/com/spp/git/Test1.java
+++ src/main/java/com/spp/git/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello1");
         System.out.println("有乱码吗？？？");
+        System.out.println("没有乱码！！！");
     }
 }
